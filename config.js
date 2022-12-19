1c467888003ecf27cbc383c2cd1178ababbec485
TakasanLab <64729168+TakasanLab@users.noreply.github.com>
2022-12-19 17:40:15 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+    sdkKey: 'YYdjlwLjfahvaypXrY5KvCiTbX1EOZdKtGMh',
+    sdkSecret: 'YGnMpzY02u3IssEsWUidKwKTpFa6mUcyX2Gv',
+    topic: 'turtlebot',
+    password: '1104',
+    name: 'yellow_robot',
+    sessionKey: '1104',
+    user_identity: 'yellow_robot'
+};
